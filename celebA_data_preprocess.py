c38164d60cbbf8da6103b91d067051b90f21f91d
Hyeonwoo Kang <znxlwm@gmail.com>
2017-07-24 14:26:58 +0900

    Add files via upload

---
+++ celebA_data_preprocess.py
@@ -0,0 +1,24 @@
+import os
+import matplotlib.pyplot as plt
+from scipy.misc import imresize
+
+# root path depends on your computer
+root = 'data/celebA/celebA/'
+save_root = 'data/resized_celebA/'
+resize_size = 64
+
+if not os.path.isdir(save_root):
+    os.mkdir(save_root)
+if not os.path.isdir(save_root + 'celebA'):
+    os.mkdir(save_root + 'celebA')
+img_list = os.listdir(root)
+
+# ten_percent = len(img_list) // 10
+
+for i in range(len(img_list)):
+    img = plt.imread(root + img_list[i])
+    img = imresize(img, (resize_size, resize_size))
+    plt.imsave(fname=save_root + 'celebA/' + img_list[i], arr=img)
+
+    if (i % 1000) == 0:
+        print('%d images complete' % i)
